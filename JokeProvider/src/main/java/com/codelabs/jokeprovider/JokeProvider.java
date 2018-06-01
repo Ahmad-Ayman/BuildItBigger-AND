a4759de35cd28f4d8e15addfa08db3097d23a3bb
Ahmed Ayman Abd El-Moneim <33450003+Ahmeda1708@users.noreply.github.com>
2018-06-01 05:55:29 -0700

    Update JokeProvider.java

--- JokeProvider/src/main/java/com/codelabs/jokeprovider/JokeProvider.java
+++ JokeProvider/src/main/java/com/codelabs/jokeprovider/JokeProvider.java
@@ -10,7 +10,12 @@ public class JokeProvider {
         jokes = new String[3];
         jokes[0] = "A SQL query goes into a bar, walks up to two tables and asks, \"Can I join you?\"";
         jokes[1] = "Q: How many prolog programmers does it take to change a lightbulb? A: Yes.";
-        jokes[2] = "Why do Java developers wear glasses? Because they can't C#";
+        jokes[2] = "Q: Why do Java developers wear glasses? A: Because they cannot C#";
+        jokes[3] = "Q. How does a computer get drunk? A: It takes screenshots.";
+        jokes[4] = "Q: Can a kangaroo jump higher than a house? A: Of course, a house does not jump at all.";
+
+Check out this really funny jokes: https://short-funny.com/#ixzz5HB53v1iA
+";
         random = new Random();
     }
 
